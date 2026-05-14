220f42656d85f9959bde65c2c2f84f76702fc723
Rashmi Das <rashmidas333@outlook.com>
2026-05-14 20:09:39 +0200

    Finalized Week 5: Transactional Checkout, Input Validation, and API Documentation

--- api/src/routers/api.js
+++ api/src/routers/api.js
@@ -1,8 +1,10 @@
 import express from "express";
 import eventsRouter from "#routers/events.js";
+import ordersRouter from "./orders.js"; 
 
 const apiRouter = express.Router();
 
 apiRouter.use("/events", eventsRouter);
-
+apiRouter.use("/orders", ordersRouter);
+apiRouter.use("/cart", ordersRouter);
 export default apiRouter;
