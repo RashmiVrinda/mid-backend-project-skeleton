cb58670e4787eab0fca3549acedf18d374d94b3c
Rashmi Das <rashmidas333@outlook.com>
2026-05-21 12:30:47 +0200

    added redirected route in index.js

--- api/src/routers/index.js
+++ api/src/routers/index.js
@@ -3,6 +3,10 @@ import apiRouter from "#routers/api.js";
 import authRouter from "./auth.mjs";
 const rootRouter = express.Router();
 
+rootRouter.get("/", (req, res) => {
+  res.redirect("/docs");
+});
+
 rootRouter.use("/api", apiRouter);
 rootRouter.use("/auth", authRouter);
 export default rootRouter;
